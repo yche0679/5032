ae31619eb3781f14b2ae2e14f9a0828fc3caf888
yche0679 <yche0679@student.monash.edu>
2024-07-28 18:07:53 +0800

    Add files via upload

---
+++ 4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_4.Startup))]
+namespace _4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
