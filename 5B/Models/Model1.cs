a01affb5f2b9fdb8c57f27d494b67124ac68b35b
yche0679 <yche0679@student.monash.edu>
2024-07-28 18:19:01 +0800

    Add files via upload

---
+++ 5B/Models/Model1.cs
@@ -0,0 +1,26 @@
+using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+
+namespace _5B.Models
+{
+    public partial class Model1 : DbContext
+    {
+        public Model1()
+            : base("name=Model1")
+        {
+        }
+
+        public virtual DbSet<Student> Students { get; set; }
+        public virtual DbSet<Unit> Units { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Student>()
+                .HasMany(e => e.Units)
+                .WithRequired(e => e.Student)
+                .WillCascadeOnDelete(false);
+        }
+    }
+}
---
+++ 5B/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_5B.Startup))]
+namespace _5B
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
